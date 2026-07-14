ef498c6e9c7e384c18de7946acfe9830f24d6f2b
Emma Champion <277982908+EmmaChampion@users.noreply.github.com>
2026-07-13 21:34:40 -0400

    Write logic for human choice

--- javascript.js
+++ javascript.js
@@ -4,19 +4,24 @@ function getComputerChoice() {
     // 2 = Scissors
     choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
-        return "Rock";
+        return "rock";
     }
     else if (choice === 1) {
-        return "Paper";
+        return "paper";
     }
     else if (choice === 2) {
-        return "Scissors";
+        return "scissors";
     }
     else {
         return "Error";
     }
 }
 
-for (let i = 0; i < 10; i++) {
-    console.log(getComputerChoice());
+function getHumanChoice() {
+    choice = prompt("Rock, paper, scissors, shoot! What is your choice?");
+    return choice.toLowerCase();
+}
+
+for (let i = 0; i < 3; i++) {
+    console.log(getHumanChoice());
 }
